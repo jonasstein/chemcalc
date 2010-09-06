798c68759709d4b2ab909aa66fda24a93a1a2714
Jonas Stein <news@jonasstein.de>
2010-09-06 03:22:25 +0200

    read format...

--- read.f95
+++ read.f95
@@ -1,15 +1,16 @@
 program readchem
   implicit none
 
-  character(len=50) :: chemeq = "HCl23HO23JPIS24"
+  character(len=50) :: chemeq = "HCl23HO23JPIS24G"
   character(len=1) :: this_char
 
   character, dimension(50,3) :: eqsymbols
   integer, dimension(50) :: eqquantity
 
   !  real, dimension(10) :: Faktor
-  integer :: position = 1
-
+  integer :: position
+  integer :: numberofsymbols = 0 
+  integer :: lengthofeqstring = 0
 
   ! functions
   logical :: isuppercase
@@ -19,28 +20,62 @@ program readchem
 
 
   write(*,*) "enter chemical equation:"
-  ! read(*,*) Formel
+  ! read(*,*) chemeq
 
 
+  ! ============================
+  ! count chemical symbols
+  ! ============================
   write(*,*) len(chemeq)
+  do position = 1, len(chemeq)
+     this_char = chemeq(position:position)  
+     if (this_char == "") then
+        ! no more data in equation string
+        exit
+     elseif (isuppercase(this_char)) then
+        numberofsymbols = numberofsymbols + 1
+     end if
+       lengthofeqstring = position
+  end do
+  write(*,*) numberofsymbols, "chemical symbols detected"
 
 
-  do while (position < 10)
+  ! ============================
+  ! parse the equation string
+  ! ============================
+
+  do position = 1, lengthofeqstring
      this_char = chemeq(position:position)  
      write(*,*) position, this_char, ichar(this_char)
-     position = position + 1 
 
      if (isuppercase(this_char)) then
         write(*,*) "das war gross"
+     
      elseif(islowercase(this_char)) then 
         write(*,*) "das war klein"
      elseif(isdigit(this_char)) then 
         write(*,*) "das war zahl"
-
+        eqquantity(position) = strtoint(this_char)
      else
         write(*,*) "EE: could not parse equation"
         exit
      end if
+
+
+!!$      CHARACTER*8 DATE
+!!$           INTEGER MONTH, DAY, YEAR
+!!$
+!!$           INTEGER MONTH2, DAY2, YEAR2
+!!$           MONTH = 7
+!!$           DAY = 4
+!!$           YEAR = 93
+!!$
+!!$           WRITE (DATE,10) MONTH, DAY, YEAR
+!!$        10 FORMAT (I2,'/',I2,'/',I2)
+!!$           READ (DATE,20) MONTH2, DAY2, YEAR2
+!!$       20  FORMAT (I2,1X,I2,1X,I2)
+
+
 
 
   end do
